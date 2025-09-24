b6caace9a31a94f08527aae75d91ef79e572c39b
MARCELO VIEIRA DA SILVA <25132416@CFP127ALUNO.LOCAL>
2025-09-24 08:56:45 -0300

    funcoes criadas

---
+++ modulo/funcoes.js
@@ -0,0 +1,29 @@
+/* *********************************************************************
+* Objetivo: Arquivo de funções para gerenciar a API Whatsapp
+* Data: 24/09/2025
+* Autor: Marcelo Vieira
+* Versão: 1.0
+* **********************************************************************/
+
+const MESSAGE_ERRO = {status: false, status_code: 500, development: 'Marcelo Vieira'}
+const MESSAGE_CABECALHO = {status: true, status_code: 200, development: 'Marcelo Vieira'}
+
+const dados = require('./contatos.js')
+
+const getAllDados = () => {
+
+}
+
+const getDadosUsersProfile = () => {}
+
+const getDadosUsers = () => {}
+
+const getAllUserMessages = () => {}
+
+const getChatUserWithContact = () => {}
+
+const getSearchByKeyword = () => {}
+
+module.exports = {
+    getAllDados
+}
