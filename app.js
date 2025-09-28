4e74d0fc283c1a47fdb6569de60433d5f27aa1b6
Marcelo Vieira <vieiramarcelo.vds@gmail.com>
2025-09-28 10:12:29 -0300

    iniciando o codigo do server

--- app.js
+++ app.js
@@ -12,3 +12,21 @@ const cors = require('cors') //responsavel pelas permissões da API
 //import do arquivo de funções
 const dados = require('./modulo/funcoes')
 
+//retorna a porta do servidor atual ou colocamos uma porta local
+const PORT = process.PORT || 8080
+
+//criando uma instancia de uma classe do express
+const app = express()
+
+//configuração de permissões
+app.use((request, response, next)=>{
+
+    //servidor de origem da API
+    response.header('Access-Control-Allow-Origin', '*') 
+    //verbos permitidos na API
+    response.header('Access-Control-Allow-Methods', 'GET') 
+    //carrega as configurações no CORS da API (Cors é como se fosse um firewall da API)
+    app.use(cors())
+    //carregar os próximos endpoints
+    next()
+})
